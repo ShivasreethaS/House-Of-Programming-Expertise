ea6cae24c8724d299948951dcfd609a980b3721e
ShivasreethaS <shivasreetha.selvarasu@gmail.com>
2025-09-18 14:30:28 +0530

    Create ifTrick.java

---
+++ ifTrick.java
@@ -0,0 +1,12 @@
+// "static void main" must be defined in a public class.
+import java.util.*;
+public class ifTrick {
+    public static void main(String[] args) {
+        if(System.out.printf("Ice")==null){
+            System.out.print("Ice");
+        }
+        else{
+            System.out.print(" Cream");
+        }
+    }
+}
