8ff991e9133b470aed96bdb2f027ecf52f36df45
ShivasreethaS <shivasreetha.selvarasu@gmail.com>
2025-09-19 10:55:38 +0530

    Update hourGlass.java

--- hourGlass.java
+++ hourGlass.java
@@ -1,9 +1,8 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
-
-    public static void hourGlass(String[] args) {
+public class hourGlass{
+    public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc=new Scanner(System.in);
         int m,n;
