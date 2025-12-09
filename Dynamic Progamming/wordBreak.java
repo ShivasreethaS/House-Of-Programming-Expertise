e7cffc131f694bf2a7d161f527356b38dc6743f6
ShivasreethaS <shivasreetha.selvarasu@gmail.com>
2025-12-09 11:25:54 +0530

    Update wordBreak.java

--- Dynamic Progamming/wordBreak.java
+++ Dynamic Progamming/wordBreak.java
@@ -1,11 +1,13 @@
+
 import java.util.*;
-class wordBreak {
+class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String s=sc.nextLine();
-        int n=sc.nextInt();
+        int n1=sc.nextInt();
+        int n=s.length();
         String[] wordDict=new String[n];
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n1;i++){
             wordDict[i]=sc.next();
         }
         boolean[] dp=new boolean[n+1];
