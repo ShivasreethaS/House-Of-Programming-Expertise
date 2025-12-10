3a800b481d5aefda3442beb7dd2ce4384bdfe80e
ShivasreethaS <shivasreetha.selvarasu@gmail.com>
2025-12-10 10:41:26 +0530

    Create fuelPrice.java

---
+++ Dynamic Progamming/fuelPrice.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class fuelPrice {
+    public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    int c=5;//sc.nextInt();//cost total nos
+    int amt=10;//sc.nextInt();//total money i have
+    int[] cost={1,1,4,5,9};//new int[c];
+    int[] fuel={1,2,2,5,15};//new int[c];
+    // for(int i=0;i<c;i++){
+    //     cost[i]=sc.nextInt();
+    //     fuel[i]=sc.nextInt();
+    // }
+    int[][] dp=new int[c+1][amt+1];
+    for(int i=1;i<=c;i++){
+        for(int j=1;j<=amt;j++){
+            if(j>=cost[i-1]){
+                dp[i][j]=Math.max(dp[i-1][j],dp[i-1][j-cost[i-1]]+fuel[i-1]);
+            }
+            else{
+                dp[i][j]=dp[i-1][j];}
+        }
+    }
+    System.out.println(dp[c][amt]);
+    }
+}
+
